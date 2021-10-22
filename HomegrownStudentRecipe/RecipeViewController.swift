a41f70fef3b34907181c1987c51910d8ad9cd5ac
Leebumju <69973158+Leebumju@users.noreply.github.com>
2021-10-22 11:05:32 +0900

    v5. third view controller complete need data survey

--- HomegrownStudentRecipe/RecipeViewController.swift
+++ HomegrownStudentRecipe/RecipeViewController.swift
@@ -11,7 +11,9 @@ class RecipeViewController: UIViewController {
 
     @IBOutlet weak var foodImgView: UIImageView!
     @IBOutlet weak var recipeName: UILabel!
-    //@IBOutlet weak var etc: UILabel!
+    @IBOutlet weak var material1: UILabel!
+    //material1.numberOfLines = 0
+    
     
     let chickenList = ["chicken curry","chicken salad"]
     let pastaList = ["cream pasta", "tomato pasta"]
@@ -24,13 +26,23 @@ class RecipeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
     func updateUI() {
         if let name = self.name{
             let img = UIImage(named: "\(name).jpg")
+            //let mat: String = "sour cream"
             foodImgView.image=img
             recipeName.text = name
+            if(name == "chicken curry"){
+                material1.text = "sour cream 1 spoon \ngochujang 2 spoon"
+                material1.numberOfLines = 2
+            }else if(name == "cream pasta"){
+                material1.text = "bulgogi source 1 spoon \nwhipping source 4spoon \nsour cream 3spoon"
+                material1.numberOfLines = 3
+            }
         }
     }
+    
     
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
